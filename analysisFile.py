c858536900e4fe5b1b0d4d0e6afd75b32e551a24
Parhelia93 <91428441+Parhelia93@users.noreply.github.com>
2022-04-12 20:10:52 +0300

    Update analysisFile.py

--- analysisFile.py
+++ analysisFile.py
@@ -1,64 +1,59 @@
-#Must be ini file, where we can add path main folder and path slave folder(s). We can add list of file extension
-#Script make folder for result, and make txt file with name current date
-
 import os
 from datetime import datetime
 
+INI_FILE_NAME = 'ini_analyzeFile.txt'
 
 def convert_date(timestamp):
     d = datetime.fromtimestamp (timestamp)
     formated_date = d.strftime('%d %b %Y, %H:%M:%S')
     return formated_date
 
-dict_one = {}
-dict_two = {}
 
-with os.scandir('folder_one') as dir_contents:
-    for entry in dir_contents:
-        info = entry.stat()
-        if entry.is_file():
-            dict_one[entry.name] = (info.st_size,info.st_mtime)
-            
-        
-with os.scandir('folder_two') as dir_contents:
-    for entry in dir_contents:
-        info = entry.stat()
-        if entry.is_file():
-            dict_two[entry.name] = (info.st_size,info.st_mtime)
-            
+def read_folder_files(folderPath):
+    result = {}
+    try:
+        with os.scandir(folderPath) as dir_contents:
+            for entry in dir_contents:
+                info = entry.stat()
+                if entry.is_file():
+                    result[entry.name] = (info.st_size,info.st_mtime)
+        return result
+    except FileNotFoundError:
+        print('The system cannot find the path specified:', folderPath)
+    except:
+        print('General Problem with workin directory')          
 
-def compareTuple(d1, d2):
+       
+def compare_tuple(tuple1, tuple2):
+    same, d1_newer, d2_newer = 1,2,3
     result = [0,0]
-    #result = {FileName:[size result , time result]}
-    for i in range(len(d1)):
-        if d1[i]==d2[i]:
-            result[i]=1#Same
-        elif d1[i]>d2[i]:
-            result[i]=2#d1 newer version
+    for i in range(len(tuple1)):
+        if tuple1[i]==tuple2[i]:
+            result[i]=same
+        elif tuple1[i]>tuple2[i]:
+            result[i]=d1_newer
         else:
-            result[i]=3#d2 newer version
+            result[i]=d2_newer
+    return result
 
-    return tuple(result)
-
-def compareDict(d1,d2):
+def compare_dict(d1,d2):
     #result {FileName: [exist, size, time]}
-    result = [0,0,0]
-    outD = {}
-    for k,v in d1.items():
-        if k in d2.keys():
-            result[0]=1
-            res = compareTuple(v,d2.get(k))
+    exist, exist_path_one, exist_path_two = 1,2,3
+    result_dict = dict()
+    for key_d1,value_d1 in d1.items():
+        result = [0,0,0] #
+        if key_d1 in d2.keys():
+            result[0]=exist
+            res = compare_tuple(value_d1,d2.get(key_d1))
             result[1:]=res
-            outD[k]=result
-            result = [0,0,0]
+            result_dict[key_d1]=result
         else:
-            outD[k] = [2,0,0]
+            result_dict[key_d1] = [exist_path_one,0,0]
             
-    for k,v in d2.items():
-        if k not in d1.keys():
-            outD[k] = [3,0,0]
-                
-    return outD
+    for key_d2 in d2.keys():
+        if key_d2 not in d1.keys():
+            result_dict[key_d2] = [exist_path_two,0,0]           
+    return result_dict
 
 def printResult(resultDict):
     for k,v in resultDict.items():
@@ -77,4 +72,25 @@ def printResult(resultDict):
         elif v[0]==1 and v[2]==1:
             print(k, 'exist in both folder and equal')
             continue
-        
+
+
+def read_ini_file(iniName):
+    try:
+        with open(iniName, encoding='utf-8') as file:
+            s = file.readlines()
+            result = {}
+            for i in s:
+                key, value = i.split('=')
+                result[key]=value.replace("\n","")
+            return result
+    except:
+        print('Some problem with open ini file')
+
+
+ini_date = read_ini_file(INI_FILE_NAME)
+dict_one = read_folder_files(ini_date.get('folder_path_one'))
+dict_two = read_folder_files(ini_date.get('folder_path_two'))
+
+result = compare_dict(dict_one, dict_two)
+print(result)
+
